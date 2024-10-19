dc37ad256d11c45218f4dd111d9e0858db532f3c
GHaxZ <ghax.z.yt@proton.me>
2024-10-19 23:31:22 +0200

    Update TODOs

--- src/main.rs
+++ src/main.rs
@@ -7,8 +7,11 @@ mod model;
 mod ui;
 
 //  TODO:
-//  Make it possible to select playback device, or automatically choose one when only one is
-//  currently available
+//  Add configuration support for current song formatting etc.
+//  Add feature for playing users playlists (with selection or search)
+//  Output current song ascii cover art
+//
+//  FIX: Workaround in client playback device functionality
 
 #[tokio::main]
 async fn main() -> Result<()> {
